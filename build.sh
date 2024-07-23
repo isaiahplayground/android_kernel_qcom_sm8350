b484cb087337e0ed8038c3cb2807266eaf5d2f8b
Isaiah <lqlp0011@gmail.com>
2024-07-23 12:21:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 # Copyright (C) 2022-2023 Mar Yvan D.
 
 # Dependency preparation
-git clone --depth=1 https://github.com/likkai/AnyKernel3.git -b lisa AnyKernel
 sudo apt install bc -y
 sudo apt-get install device-tree-compiler -y
 
@@ -54,7 +53,7 @@ function sendinfo() {
         -d text="<b>Laboratory Machine: Build Triggered</b>%0A<b>Docker: </b><code>$DISTRO</code>%0A<b>Build Date: </b><code>${DATE}</code>%0A<b>Device: </b><code>${DEVICE} (${CODENAME})</code>%0A<b>Kernel Version: </b><code>$(make kernelversion 2>/dev/null)</code>%0A<b>Build Type: </b><code>${TYPE}</code>%0A<b>Compiler: </b><code>${COMPILER_NAME}</code>%0A<b>Linker: </b><code>${LINKER_NAME}</code>%0A<b>Zip Name: </b><code>${KVERSION}</code>%0A<b>Branch: </b><code>$(git rev-parse --abbrev-ref HEAD)</code>%0A<b>Last Commit Details: </b><a href='${REPO_URL}/commit/${COMMIT_HASH}'>${COMMIT_HASH}</a> <code>($(git log --pretty=format:'%s' -1))</code>"
 }
 function push() {
-    cd AnyKernel
+    cd AnyKernel3
     ZIP=$(echo *.zip)
     curl -F document=@$ZIP "https://api.telegram.org/bot${BOT_ID}/sendDocument" \
         -F chat_id="$CHAT_ID" \
@@ -77,36 +76,44 @@ function compile() {
     export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
     export LLVM=1
     export LLVM_IAS=1
-    make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip kernel="out/arch/arm64/boot/Image.gz-dtb" dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb" dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo" Image.gz-dtb dtb dtbo.img 3>&1 | tee log.txt
-
-    if ! [ -a "$IMAGE" ]; then
-        finerr
-        exit 1
-    fi
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-
-    if ! [ -a "$DTB" ]; then
-        finerr
-        exit 1
-    fi
-    cp out/arch/arm64/boot/dtb AnyKernel
+    make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip 
     
-    if ! [ -a "$DTBO" ]; then
+    kernel="out/arch/arm64/boot/Image.gz-dtb"
+    dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
+    dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
+
+    if ! [ -a "$IMAGE" ] || [ -a "$DTB" ] || [ -a "$DTBO" ] ; then
         finerr
         exit 1
     fi
-    cp out/arch/arm64/boot/dtbo.img AnyKernel
 }
-# Zipping
-function zipping() {
-    cd AnyKernel || exit 1
-    zip -r "${KVERSION}.zip" . -x ".git*" -x "README.md" -x "*.zip"
-    cd ..
-}
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -d "$AK3_DIR" ]; then
+	cp -r $AK3_DIR AnyKernel3
+	git -C AnyKernel3 checkout lisa &> /dev/null
+elif ! git clone -q https://github.com/likkai/AnyKernel3 -b lisa; then
+	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+	exit 1
+fi
+cp $kernel AnyKernel3
+cp $dtb AnyKernel3/dtb
+python2 scripts/dtc/libfdt/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $dtbo
+cp $(find out/modules/lib/modules/5.4* -name '*.ko') AnyKernel3/modules/vendor/lib/modules/
+cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} AnyKernel3/modules/vendor/lib/modules
+cp out/modules/lib/modules/5.4*/modules.order AnyKernel3/modules/vendor/lib/modules/modules.load
+sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' AnyKernel3/modules/vendor/lib/modules/modules.dep
+sed -i 's/.*\///g' AnyKernel3/modules/vendor/lib/modules/modules.load
+rm -rf out/arch/arm64/boot out/modules
+cd AnyKernel3
+zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+cd ..
+rm -rf AnyKernel3
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "Zip: $ZIPNAME"
+
 publicinfo
 sendinfo
 compile
-zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
 push
