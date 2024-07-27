d87b56c797fb28517a71c5ed1a1facb67c1f6b91
Isaiah <lqlp0011@gmail.com>
2024-07-27 18:30:14 +0800

    Redoing everything

--- build.sh
+++ build.sh
@@ -1,104 +1,56 @@
-#!/usr/bin/env bash
-# Build Script for Quickscrap
-# Copyright (C) 2022-2023 Mar Yvan D.
+#!/bin/bash
 
-# Dependency preparation
-sudo apt install bc -y
-sudo apt-get install device-tree-compiler -y
+if [ ! -d "$HOME/tc/proton_clang" ]
+	then
+		echo -e "\nCloning clang...\n"
+		git clone --single-branch https://github.com/stormbreaker-project/stormbreaker-clang -b 11.x "$HOME"/tc/proton_clang
+fi
 
-# Main Variables
-KDIR=$(pwd)
-DATE=$(date +%d-%h-%Y-%R:%S | sed "s/:/./g")
-START=$(date +"%s")
-TCDIR=$(pwd)/toolchains/clang
-DTB=out/arch/arm64/boot/dtb
-DTBO=out/arch/arm64/boot/dtbo.img
-IMAGE=out/arch/arm64/boot/Image.gz-dtb
+SECONDS=0
+ZIPNAME="Quickscrap-lisa-$(date '+%Y%m%d-%H%M').zip"
 
-# Naming Variables
-KNAME="Quickscrap"
-VERSION="v2.0"
-CODENAME="lisa"
-MIN_HEAD=$(git rev-parse HEAD)
-export KVERSION="${KNAME}-${VERSION}-${CODENAME}-$(echo ${MIN_HEAD:0:8})"
+export PATH="$HOME/tc/proton_clang/bin:$PATH"
+export STRIP="$HOME/tc/proton_clang/aarch64-linux-gnu/bin/strip"
+export KBUILD_COMPILER_STRING=$("$HOME"/tc/proton_clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
-# Build Information
-LINKER=ld.lld
-export COMPILER_NAME="$(${TCDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-export LINKER_NAME="$("${TCDIR}"/bin/${LINKER} --version | head -n 1 | sed 's/(compatible with [^)]*)//' | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
+export KBUILD_BUILD_HOST=workspace
 export KBUILD_BUILD_USER=isaiahscape
-export KBUILD_BUILD_HOST=runner
-export DEVICE="Xiaomi 11 Lite 5G NE / 11 LE"
-export CODENAME="lisa"
-export TYPE="Stable"
-export DISTRO=$(source /etc/os-release && echo "${NAME}")
 
-# Telegram Integration Variables
-CHAT_ID="-1001865106728"
-PUBCHAT_ID="1865106728"
-BOT_ID="7478955642:AAGGCsWTxY9VXYrzi_I0biNuZLMMf_2DDPk"
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+	echo "Cleaned output folder"
+fi
 
-function publicinfo() {
-    curl -s -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage" \
-        -d chat_id="$PUBCHAT_ID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>Automated build started for ${DEVICE} (${CODENAME})</b>"
-}
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage" \
-        -d chat_id="$CHAT_ID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>Laboratory Machine: Build Triggered</b>%0A<b>Docker: </b><code>$DISTRO</code>%0A<b>Build Date: </b><code>${DATE}</code>%0A<b>Device: </b><code>${DEVICE} (${CODENAME})</code>%0A<b>Kernel Version: </b><code>$(make kernelversion 2>/dev/null)</code>%0A<b>Build Type: </b><code>${TYPE}</code>%0A<b>Compiler: </b><code>${COMPILER_NAME}</code>%0A<b>Linker: </b><code>${LINKER_NAME}</code>%0A<b>Zip Name: </b><code>${KVERSION}</code>%0A<b>Branch: </b><code>$(git rev-parse --abbrev-ref HEAD)</code>%0A<b>Last Commit Details: </b><a href='${REPO_URL}/commit/${COMMIT_HASH}'>${COMMIT_HASH}</a> <code>($(git log --pretty=format:'%s' -1))</code>"
-}
-function push() {
-    cd AnyKernel3
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot${BOT_ID}/sendDocument" \
-        -F chat_id="$CHAT_ID" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds. | <b>Compiled with: ${COMPILER_NAME} + ${LINKER_NAME}.</b>"
-}
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage" \
-        -d chat_id="$CHAT_ID" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="Compilation failed, please check build logs for errors."
-    exit 1
-}
-function compile() {
-    make O=out ARCH=arm64 vendor/lahaina-qgki_defconfig vendor/debugfs.config vendor/xiaomi_QGKI.config vendor/lisa_QGKI.config
-    export PATH=${TCDIR}/bin/:/usr/bin/:${PATH}
-    export CROSS_COMPILE=aarch64-linux-gnu-
-    export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-    export LLVM=1
-    export LLVM_IAS=1
-    make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip 
-    
-    kernel="out/arch/arm64/boot/Image.gz-dtb"
-    dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
-    dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
+mkdir -p out
 
-    if ! [ -a "$IMAGE" ] || [ -a "$DTB" ] || [ -a "$DTBO" ] ; then
-        finerr
-        exit 1
-    fi
-}
+# make O=out clean && make O=out mrproper
+ARCH=arm64 scripts/kconfig/merge_config.sh -O out arch/arm64/configs/vendor/lahaina-qgki_defconfig \
+                                                  arch/arm64/configs/vendor/xiaomi_QGKI.config \
+                                                  arch/arm64/configs/vendor/lisa_QGKI.config
 
-publicinfo
-sendinfo
-compile
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-if [ -d "$AK3_DIR" ]; then
-	cp -r $AK3_DIR AnyKernel3
-	git -C AnyKernel3 checkout lisa &> /dev/null
-elif ! git clone -q https://github.com/likkai/AnyKernel3 -b lisa; then
-	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+echo -e "\nStarting compilation...\n"
+
+MAKE_PARAMS="O=out ARCH=arm64 AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump \
+             CC=clang STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-"
+
+make -j$(nproc --all) $MAKE_PARAMS || exit $?
+make -j$(nproc --all) $MAKE_PARAMS INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+
+kernel="out/arch/arm64/boot/Image"
+dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
+dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
+
+if [ ! -f "$kernel" ] || [ ! -f "$dtb" ] || [ ! -f "$dtbo" ]; then
+	echo -e "\nCompilation failed!"
 	exit 1
 fi
+
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+
+git clone -q https://github.com/ItsVixano/AnyKernel3 -b lisa-aosp
 cp $kernel AnyKernel3
 cp $dtb AnyKernel3/dtb
 python2 scripts/dtc/libfdt/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $dtbo
@@ -114,6 +66,3 @@ cd ..
 rm -rf AnyKernel3
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
-END=$(date +"%s")
-DIFF=$(($END - $START))
-push
